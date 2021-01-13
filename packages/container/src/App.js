fb09a14325b89d508c20d1ecbf237fdb08d956f0
SEBASTIEN BEAUJARD <sebastien.beaujard@gmail.com>
2021-01-13 17:37:40 +0100

    Add M D

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,7 +7,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Container 4</h1>
+      <h1>Container</h1>
       <hr />
       <MarketingApp />
     </div>
