a33a625b4dd20bf155b907416a31914750c96718
SEBASTIEN BEAUJARD <sebastien.beaujard@gmail.com>
2021-01-13 18:18:27 +0100

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 
 export default () => {
   return (
-    <div>
-      <h1>Container DEV</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
