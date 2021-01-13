028251be280fa69ea3d94e3fedcb98466c57c789
SEBASTIEN BEAUJARD <sebastien.beaujard@gmail.com>
2021-01-13 17:50:04 +0100

    F1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,7 +7,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Container</h1>
+      <h1>Container DEV</h1>
       <hr />
       <MarketingApp />
     </div>
