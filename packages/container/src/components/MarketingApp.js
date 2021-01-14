96c45ce4ed9a3082c21f3116d07f04fb96e03b53
SEBASTIEN BEAUJARD <sebastien.beaujard@gmail.com>
2021-01-14 09:39:56 +0100

    Manage navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,21 @@
 import { mount } from 'marketing/MarketingApp'
 import React, { useRef, useEffect } from 'react'
-
+import { useHistory } from 'react-router-dom'
 // It was possible that MarkertingApp to be a react component and import it like it but 
 // with this way, MarkertingApp could be another type of component in the future 
 export default () => {
     const ref = useRef(null)
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    })
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPathname)
+                    history.push(nextPathname);
+            }
+        });
+        history.listen(onParentNavigate);
+    }, [])
     return <div ref={ref} />;
 }
